c363757a464f5ca150f5d6d31bb3e6411fd45818
Mika1212 <ilravgaz03@mail.ru>
2022-10-14 11:05:58 +0300

    Creating an array of 25 photos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-function getrandomPositiveNum (leftBorder, rightBorder) {
+
+function getRandomPositiveNum (leftBorder, rightBorder) {
   if (leftBorder < 0 || rightBorder < 0) {
     return 0;
   }
@@ -9,8 +10,42 @@ function checkLength (line, maxLength) {
   return line.length <= maxLength;
 }
 
-// eslint-disable-next-line no-console
-console.log(getrandomPositiveNum(1000, 1001));
-// eslint-disable-next-line no-console
-console.log(checkLength('aaaa', 10));
 
+const COMMENTS_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Димон',
+  'Васян',
+  'Петька',
+  'Славич',
+  'Ромыч',
+  'Салыч',
+  'Сер',
+  'Лол'
+];
+
+const createComments = () => Array.from({length: getRandomPositiveNum(0, 100)}).map((value, index) => ({
+  id: index + 1,
+  avatar: `img/avatar-${getRandomPositiveNum(1, 6)}.svg`,
+  message: COMMENTS_MESSAGE[getRandomPositiveNum(0, COMMENTS_MESSAGE.length - 1)],
+  name: NAMES[getRandomPositiveNum(0, NAMES.length - 1)],
+}));
+
+
+const createPhotos = () => Array.from({length: 25}).map((value, index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  description: COMMENTS_MESSAGE[getRandomPositiveNum(0, COMMENTS_MESSAGE.length - 1)],
+  likes: getRandomPositiveNum(0, 2000),
+  comments: createComments(),
+}));
+
+createPhotos();
+checkLength('aaaaaaaaaaaaa', 6);

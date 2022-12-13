dd1c9c4f72fdbd4a4c37160d081e82bef714b4eb
Mika1212 <ilravgaz03@mail.ru>
2022-12-13 18:45:33 +0300

    Task 8.2

--- js/data.js
+++ js/data.js
@@ -4,10 +4,9 @@ import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 const PHOTOS_NUMBER = 25;
 const MIN_LIKES_NUMBER  = 15;
 const MAX_LIKES_NUMBER  = 200;
-const MAX_COMMENTS_NUMBER  = 100;
 const MAX_AVATAR_NUM = 6;
 
-const MESSAGES = [
+const COMMENTS_MESSAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -16,29 +15,25 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-let commentIndex = 0;
-let photoDescriptionIndex = 0;
-
-const getCommentId = () => commentIndex++;
-
-const getPhotoDescriptionId = () => photoDescriptionIndex++;
-
-const createComment = () => ({
-  id: getCommentId(),
+const createComments = () => Array.from({length: getRandomPositiveNum(0, 100)}).map((value, index) => ({
+  id: index + 1,
   avatar: `img/avatar-${getRandomPositiveNum(1, MAX_AVATAR_NUM)}.svg`,
-  message: MESSAGES[getRandomPositiveNum(0, MESSAGES.length-1)],
+  message: COMMENTS_MESSAGE[getRandomPositiveNum(0, COMMENTS_MESSAGE.length - 1)],
   name: faker.name.firstName()
-});
+}));
 
-const createPhotoDescription = () => ({
-  id: getPhotoDescriptionId(),
-  url: `photos/${photoDescriptionIndex}.jpg`,
-  description: faker.lorem.paragraph(),
+
+const createPhotos = () => Array.from({length: PHOTOS_NUMBER}).map((value, index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  description: COMMENTS_MESSAGE[getRandomPositiveNum(0, COMMENTS_MESSAGE.length - 1)],
   likes: getRandomPositiveNum(MIN_LIKES_NUMBER, MAX_LIKES_NUMBER),
-  comments: Array.from({length: getRandomPositiveNum(1,MAX_COMMENTS_NUMBER)}, createComment)
-});
+  comments: createComments(),
+}));
 
-const arrayOfPhotos = Array.from({length: PHOTOS_NUMBER}, createPhotoDescription);
+const arrayOfPhotos = createPhotos();
 
-checkLength('aaaa', 3);
-export{arrayOfPhotos};
+createPhotos();
+checkLength('aaaaaaaaaaaaa', 6);
+
+export {arrayOfPhotos};

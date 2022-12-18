d7fb0071efab310ec88051631cb53742de309bc4
Mika1212 <ilravgaz03@mail.ru>
2022-12-18 19:17:57 +0300

    Task 12.1

--- js/util.js
+++ js/util.js
@@ -1,3 +1,24 @@
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const throttle = (callback, delayBetweenFrames) => {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+};
+
 function getRandomPositiveNum (leftBorder, rightBorder) {
   if (leftBorder < 0 || rightBorder < 0) {
     return 0;
@@ -11,4 +32,4 @@ function checkLength (line, maxLength) {
 
 const isEscapeKey = (key) => key === 'Escape';
 
-export {getRandomPositiveNum, checkLength, isEscapeKey};
+export {getRandomPositiveNum, checkLength, isEscapeKey, debounce, throttle};
